2c9f8b8fa3f62b537be5953921ac301503d3bf5f
Kansal002 <111696854+Kansal002@users.noreply.github.com>
2023-05-31 00:56:14 +0530

    my first commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,32 +1,57 @@
+import { useState } from 'react';
 import './App.css';
+// import Accordion from './components/Accordion';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+  // import {  // router se ham bina page ko reload kiye and uske kuch components change kr skte hai. ex- like navbar nahi hata  
+  //   BrowserRouter as Router, // react router sahi se kaam nahi krta hai github pages ke saath 
+  //   Routes,
+  //   Route,
+  //   Link
+  // } from "react-router-dom";
 function App() {
+  const [alert,setAlert]=useState(null);
+  const showAlert = (message,type)=>{
+     setAlert({
+      msg:message,
+      type:type
+     })
+     setTimeout(() => {
+      setAlert(null);
+     }, 1400);
+  }
+  const [mode,setMode] = useState('light');// dark mode enable or not
+  const [text,updateText]=useState("Enable Dark Mode");
+  const changeColor=()=>{  
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#542754';
+      updateText("Disable Dark Mode");
+      showAlert("Dark mode has been enabled","success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      updateText("Enable Dark Mode");
+      showAlert("Light mode has been enabled","success");
+    }
+  }
   return (
     // return ke baad jo hota hai use JSX khte hai. Basically ye HTML hi hai on the face of js 
     // <></>  esse tum aur bhi divs bana skte ho
-     <> 
-       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">TextUtils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-        
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+    <> 
+        {/* <Router>  */}
+        <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} changeColor={changeColor} text={text}/>
+      <Alert alert = {alert}/>  
+      <div className="container my-3">
+      <TextForm heading = "Enter the text to analyze below" mode={mode} showAlert={showAlert} />
+        {/* <Routes>    */}
+          {/* <Route path="/about" element={<Accordion/>} /> about me router use ho raha esliye abhi ke liye comment out kr dia
+          <Route path="/" element={<TextForm heading = "Enter the text to analyze below" mode={mode} showAlert={showAlert} />} /> */}
+        {/* </Routes> */}
+      </div>
+  {/* //  </Router> */}
     </>
     // react single page application hoti hai
    
---
+++ src/components/TextForm.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react'
+// hooks bina class banae class ke feature use karne dete hai 
+// hooks state change krne ke liye hota hai
+export default function TextForm(props) {
+  // hooks ko function ke andr hi use kr skte hai
+    const handleUpClick = ()=>{
+        // console.log("UpperCase was clicked "); 
+        let newText=text.toUpperCase();
+        props.showAlert("Converted to UpperCase !","success");
+        setText(newText)
+    }
+    const handleLoClick = ()=>{
+        // console.log("UpperCase was clicked "); 
+        let newText=text.toLowerCase();
+        props.showAlert("Converted to LowerCase !","success");
+        setText(newText)
+    }
+    const handleClear=()=>{
+      let newText='';
+      setText(newText);
+      props.showAlert("Text Cleared!","success");
+    }
+    const handleSentence=()=>{
+      let newText = text.charAt(0).toUpperCase() + text.slice(1);
+      props.showAlert(" Converted to Sentence case!","success");
+      setText(newText);
+    }
+    const handleWords=()=>{
+      let str = text;
+      let arr = str.split(" ");
+      for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+       }
+      let str2 = arr.join(" ");
+      props.showAlert("Converted to Title case !","success");
+      setText(str2);
+    }
+    const handleOnChange = (event)=>{ // yahi events hai
+        // console.log("On change ");
+        setText(event.target.value)
+    }
+    const [text, setText] = useState(''); // have to memorize it, yahi hook hai
+    // text="new text" // wrong way to change the state
+    // setText("new value"); // right way
+  return (
+    <>
+    <div className='container' style={{color: props.mode==='dark'?'white':'black'}} >
+        <h1>{props.heading} </h1>
+        <div className="mb-3">
+        
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'? '#e8d2f0':'white', color: props.mode==='dark'?'#4a2443':'black' }} id="exampleFormControlTextarea1" rows="8"></textarea> 
+         {/*double curly braces means ki ham js ke andr object bana rahe hai  */}
+        </div>
+        <button className="btn btn-success mx-2" onClick={handleUpClick} >Convert to Upper Case</button>
+        <button className="btn btn-success mx-2" onClick={handleLoClick} >Convert to Lower Case</button>
+        <button className="btn btn-success mx-2" onClick={handleSentence} >Sentence Case</button>
+        <button className="btn btn-success mx-2" onClick={handleWords} >Title Case</button>
+        <button className="btn btn-warning mx-2" onClick={handleClear} >Clear All</button>
+
+    </div>
+    <div className="container my-4" style={{color: props.mode==='dark'?'white':'black'}} >
+      <h2>Your Text summary</h2>
+       <p>{text.split(" ").length-1} words and {text.length} characters</p>
+       <h3>Preview</h3>
+       <p>{text.length>0?text:'Enter something in the textbox above to preview it here.'}</p>
+    </div>
+
+    </>
+  )
+}
+
