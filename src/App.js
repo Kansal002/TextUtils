553d8ea477f08723fffe81295fa5eb0d30c65428
Kansal002 <111696854+Kansal002@users.noreply.github.com>
2023-06-01 00:50:45 +0530

    include router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import { useState } from 'react';
 import './App.css';
-// import Accordion from './components/Accordion';
+import Accordion from './components/Accordion';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-  // import {  // router se ham bina page ko reload kiye and uske kuch components change kr skte hai. ex- like navbar nahi hata  
-  //   BrowserRouter as Router, // react router sahi se kaam nahi krta hai github pages ke saath 
-  //   Routes,
-  //   Route,
-  //   Link
-  // } from "react-router-dom";
+  import {  // router se ham bina page ko reload kiye and uske kuch components change kr skte hai. ex- like navbar nahi hata  
+    BrowserRouter as Router, // react router sahi se kaam nahi krta hai github pages ke saath 
+    Routes,
+    Route,
+  } from "react-router-dom";
 function App() {
   const [alert,setAlert]=useState(null);
   const showAlert = (message,type)=>{
@@ -41,17 +40,16 @@ function App() {
     // return ke baad jo hota hai use JSX khte hai. Basically ye HTML hi hai on the face of js 
     // <></>  esse tum aur bhi divs bana skte ho
     <> 
-        {/* <Router>  */}
+        <Router> 
         <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} changeColor={changeColor} text={text}/>
       <Alert alert = {alert}/>  
       <div className="container my-3">
-      <TextForm heading = "Enter the text to analyze below" mode={mode} showAlert={showAlert} />
-        {/* <Routes>    */}
-          {/* <Route path="/about" element={<Accordion/>} /> about me router use ho raha esliye abhi ke liye comment out kr dia
-          <Route path="/" element={<TextForm heading = "Enter the text to analyze below" mode={mode} showAlert={showAlert} />} /> */}
-        {/* </Routes> */}
+        <Routes>   
+           <Route path="/about" element={<Accordion/>} /> 
+          <Route path="/" element={<TextForm heading = "Enter the text to analyze below" mode={mode} showAlert={showAlert} />} /> 
+        </Routes>
       </div>
-  {/* //  </Router> */}
+    </Router>
     </>
     // react single page application hoti hai
    
